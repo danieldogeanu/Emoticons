0c479aaae8800f02845b176b58c24a35b0fa46c5
Daniel Dogeanu <contact@danieldogeanu.com>
2019-01-13 18:48:17 +0200

    Added Browser Detection Package to React

--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import BrowserDetection from 'react-browser-detection';
 import './index.scss';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const browserHandler = {
+	ie: () => false,
+	default: (browser) => {
+		if (browser !== 'ie') {
+			let html = document.querySelector('html').classList;
+			let browserupgrade = document.querySelector('.browserupgrade');
+			if (html.contains('ie')) {
+				html.remove('ie');
+				html.add(browser);
+				browserupgrade.remove();
+			}
+			return <App />;
+		}
+	},
+};
+
+ReactDOM.render(
+	<BrowserDetection>{browserHandler}</BrowserDetection>,
+	document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
