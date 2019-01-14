19f304f2736c5bb5c29e936e61a32864221a5caf
Daniel Dogeanu <contact@danieldogeanu.com>
2019-01-14 13:55:51 +0200

    Fixed No Target Blank Error

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,12 +4,12 @@ import './Footer.scss';
 function Footer() {
 	return (
 		<footer className="Footer">
-			<p className="author">By: <a href="https://danieldogeanu.com" target="_blank">Daniel Dogeanu</a></p>
+			<p className="author">By: <a href="https://danieldogeanu.com" target="_blank" rel="noopener noreferrer">Daniel Dogeanu</a></p>
 			<p className="mynets">Follow Me: 
 				<span>
-					<a href="#" target="_blank">Twitter</a>
-					<a href="#" target="_blank">GitHub</a>
-					<a href="#" target="_blank">Behance</a>
+					<a href="https://twitter.com/danieldogeanu" target="_blank" rel="noopener noreferrer">Twitter</a>
+					<a href="https://github.com/danieldogeanu" target="_blank" rel="noopener noreferrer">GitHub</a>
+					<a href="https://behance.net/danieldogeanu" target="_blank" rel="noopener noreferrer">Behance</a>
 				</span>
 			</p>
 		</footer>
