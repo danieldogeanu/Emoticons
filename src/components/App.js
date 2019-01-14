5ef0d61bb4664d506c921342732f70308b4308ba
Daniel Dogeanu <contact@danieldogeanu.com>
2019-01-14 10:48:25 +0200

    Reset the App Component and Styles

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,10 @@
 import React, { Component } from 'react';
-import logo from '../images/logo.svg';
 import './App.scss';
 
 class App extends Component {
 	render() {
 		return (
-			<div className="App">
-				<header className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
-					<p>Practice React App</p>
-					<a
-						className="App-link"
-						href="https://reactjs.org"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						Learn React
-					</a>
-				</header>
-			</div>
+			<div className="App"></div>
 		);
 	}
 }
