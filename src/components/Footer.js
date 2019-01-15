7d57f7253a209eb1d9520b4f935a3793cb152e62
Daniel Dogeanu <contact@danieldogeanu.com>
2019-01-15 13:10:48 +0200

    Refactored Footer Component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import './Footer.scss';
 
+const author = { name: 'Daniel Dogeanu', url: 'https://danieldogeanu.com' };
+const social = [
+	{ name: 'Twitter', url: 'https://twitter.com/danieldogeanu' },
+	{ name: 'GitHub', url: 'https://github.com/danieldogeanu' },
+	{ name: 'Behance', url: 'https://behance.net/danieldogeanu' },
+];
+
+function Link(props) {
+	return (
+		<a href={props.data.url} 
+			target="_blank" 
+			rel="noopener noreferrer">
+			{props.data.name}
+		</a>
+	);
+}
+
 function Footer() {
 	return (
 		<footer className="Footer">
-			<p className="author">By: <a href="https://danieldogeanu.com" target="_blank" rel="noopener noreferrer">Daniel Dogeanu</a></p>
+			<p className="author">By: <Link data={author} /></p>
 			<p className="mynets">Follow Me: 
-				<span>
-					<a href="https://twitter.com/danieldogeanu" target="_blank" rel="noopener noreferrer">Twitter</a>
-					<a href="https://github.com/danieldogeanu" target="_blank" rel="noopener noreferrer">GitHub</a>
-					<a href="https://behance.net/danieldogeanu" target="_blank" rel="noopener noreferrer">Behance</a>
-				</span>
+				<span>{social.map((value) => <Link data={value} />)}</span>
 			</p>
 		</footer>
 	);
