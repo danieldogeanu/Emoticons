47964ada37bbace449f06c5361b580f93c5be464
Daniel Dogeanu <contact@danieldogeanu.com>
2019-01-20 13:17:04 +0200

    Added Method to Handle Filter Text Change to SearchBar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,10 +2,22 @@ import React, { Component } from 'react';
 import './SearchBar.scss';
 
 class SearchBar extends Component {
+	constructor(props) {
+		super(props);
+		this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
+	}
+
+	handleFilterTextChange(e) {
+		this.props.onFilterTextChange(e.target.value);
+	}
+
 	render() {
 		return (
 			<form className="SearchBar">
-				<input type="text" placeholder="Search Emoticons..." />
+				<input type="text" 
+					placeholder="Search Emoticons..."
+					value={this.props.filterText}
+					onChange={this.handleFilterTextChange} />
 			</form>
 		);
 	}
