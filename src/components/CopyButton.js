52c3a8a53a0669535fe6945363adff181a71e634
Daniel Dogeanu <contact@danieldogeanu.com>
2019-01-27 03:18:24 +0200

    Added State and onSuccess Functionality for CopyButton

--- src/components/CopyButton.js
+++ src/components/CopyButton.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react';
 import Clipboard from 'react-clipboard.js';
+import Icon from './Icon';
 import './CopyButton.scss';
 
 class CopyButton extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {isCopied: false};
+		this.onSuccess = this.onSuccess.bind(this);
+		this.copyBtn = React.createRef();
+	}
+
+	onSuccess() {
+		this.setState({isCopied: true});
+		this.copyBtn.current.element.classList.add('copied');
+		setTimeout(() => {
+			this.setState({isCopied: false});
+			this.copyBtn.current.element.classList.remove('copied');
+		}, 1500);
+	}
+
 	render() {
 		return (
 			<Clipboard
-				data-clipboard-text={this.props.emoticon}
-				className="CopyButton">
-				Copy
+				ref={this.copyBtn}
+				className="CopyButton"
+				onSuccess={this.onSuccess}
+				data-clipboard-text={this.props.emoticon}>
+				{this.state.isCopied ? (
+					<Icon name="check" />
+				) : 'Copy'}
 			</Clipboard>
 		);
 	}
