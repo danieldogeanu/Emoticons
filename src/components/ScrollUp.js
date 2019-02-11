6fd8726ce571d058d4563c65218abeded8aed74b
Daniel Dogeanu <contact@danieldogeanu.com>
2019-02-11 09:44:23 +0200

    Fixed ScrollUp Component

--- src/components/ScrollUp.js
+++ src/components/ScrollUp.js
@@ -12,12 +12,13 @@ class ScrollUp extends Component {
 	}
 
 	componentDidMount() {
-		window.addEventListener('scroll', this.handleScroll);
+		this.SimpleBarContent = document.querySelector('.simplebar-content');
+		this.SimpleBarContent.addEventListener('scroll', this.handleScroll);
 		window.addEventListener('keyup', this.handleKeyUp);
 	}
 
 	componentWillUnmount() {
-		window.removeEventListener('scroll', this.handleScroll);
+		this.SimpleBarContent.removeEventListener('scroll', this.handleScroll);
 		window.removeEventListener('keyup', this.handleKeyUp);
 	}
 
@@ -28,16 +29,16 @@ class ScrollUp extends Component {
 	}
 
 	handleScroll() {
-		let currentScroll = window.scrollY;
+		let currentScroll = this.SimpleBarContent.scrollTop;
 		let thisBtn = this.scrollUpBtn.current.classList;
 		(currentScroll > 200) ? thisBtn.add('show') : thisBtn.remove('show');
 	}
 
 	scrollToTop() {
-		let currentScroll = window.scrollY;
+		let currentScroll = this.SimpleBarContent.scrollTop;
 		if (currentScroll > 0) {
 			window.requestAnimationFrame(this.scrollToTop);
-			window.scrollTo(0, currentScroll - (currentScroll/5));
+			this.SimpleBarContent.scrollTo(0, currentScroll - (currentScroll/5));
 		}
 	}
 
