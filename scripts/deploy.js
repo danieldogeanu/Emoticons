4ff3adda25a12abc7082ac514e908a305f99428f
Daniel Dogeanu <contact@danieldogeanu.com>
2019-10-04 00:08:34 +0300

    Installed FTP Deploy

---
+++ scripts/deploy.js
@@ -0,0 +1,19 @@
+const cred = require('../credentials.json');
+const ftpDeploy = require('ftp-deploy');
+const ftp = new ftpDeploy();
+
+const config = {
+	user: cred.user,
+	password: cred.pass,
+	host: cred.host,
+	port: cred.port,
+	localRoot: './build',
+	remoteRoot: cred.root,
+	include: ['.*', '*', 'build/*'],
+	deleteRemote: true,
+	forcePasv: true,
+};
+
+ftp.deploy(config)
+	.then(res => console.log('Deployed: ', res))
+	.catch(err => console.error('Deploy Failed: ', err));
