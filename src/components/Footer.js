a2bfb0f0a513bb1c910745ae9674902a4baa0513
Daniel Dogeanu <contact@danieldogeanu.com>
2020-03-11 17:17:58 +0200

    Extracted Link from Footer to Elements

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,43 +1,36 @@
-import React from 'react';
-import Icon from '../elements/Icon';
+import React, { Component } from 'react';
+import Link from '../elements/Link';
 import '../styles/components/Footer.scss';
 
-const author = { name: 'Daniel Dogeanu', url: 'https://danieldogeanu.com' };
-const social = [
-	{ name: 'Twitter', url: 'https://twitter.com/danieldogeanu' },
-	{ name: 'GitHub', url: 'https://github.com/danieldogeanu' },
-	{ name: 'Behance', url: 'https://behance.net/danieldogeanu' },
-];
+class Footer extends Component {
+	constructor() {
+		super();
+		this.state = {
+			author: { name: 'Daniel Dogeanu', url: 'https://danieldogeanu.com' },
+			social: [
+				{ name: 'Twitter', url: 'https://twitter.com/danieldogeanu' },
+				{ name: 'GitHub', url: 'https://github.com/danieldogeanu' },
+				{ name: 'Behance', url: 'https://behance.net/danieldogeanu' },
+			]
+		};
+	}
 
-function Link(props) {
-	return (
-		<a href={props.data.url}
-			target="_blank"
-			rel="noopener noreferrer">
-			{props.social ? (
-				<span className="mynet">
-					<span className="show-for-screen-reader">{props.data.name}</span>
-					<Icon name={props.data.name.toLowerCase()} />
-				</span>
-			) : (props.data.name)}
-		</a>
-	);
-}
-
-function Footer() {
-	return (
-		<footer className="Footer">
-			<p className="author">By: <Link data={author} /></p>
-			<p className="mynets">
-				<span className="folw">Follow Me: </span>
-				<span className="wrapper">
-					{social.map((value) => {
-						return <Link key={value.name.toLowerCase()} data={value} social={true} />;
-					})}
-				</span>
-			</p>
-		</footer>
-	);
+	render() {
+		const {author, social} = this.state;
+		return (
+			<footer className="Footer">
+				<p className="author">By: <Link data={author} /></p>
+				<p className="mynets">
+					<span className="folw">Follow Me: </span>
+					<span className="wrapper">
+						{social.map((value) => {
+							return <Link key={value.name.toLowerCase()} data={value} social={true} />;
+						})}
+					</span>
+				</p>
+			</footer>
+		);
+	}
 }
 
 export default Footer;
---
+++ src/elements/Link.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Icon from './Icon';
+
+function Link(props) {
+	return (
+		<a href={props.data.url}
+			target="_blank"
+			rel="noopener noreferrer">
+			{props.social ? (
+				<span className="mynet">
+					<span className="show-for-screen-reader">{props.data.name}</span>
+					<Icon name={props.data.name.toLowerCase()} />
+				</span>
+			) : (props.data.name)}
+		</a>
+	);
+}
+
+export default Link;
