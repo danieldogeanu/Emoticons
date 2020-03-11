61a8b2a60c4e6eda42b9034ba35bf78cd2df7e7f
Daniel Dogeanu <contact@danieldogeanu.com>
2020-03-11 16:45:15 +0200

    Refactored SearchBar to Use Arrow Functions

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -16,14 +16,41 @@ class SearchBar extends Component {
 	constructor(props) {
 		super(props);
 		this.searchInput = React.createRef();
-		this.selectSearchInput = this.selectSearchInput.bind(this);
-		this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
-		this.handleClearInput = this.handleClearInput.bind(this);
-		this.handleKeyUp = this.handleKeyUp.bind(this);
-		this.handleFocus = this.handleFocus.bind(this);
-		this.handleBlur = this.handleBlur.bind(this);
 	}
-	
+
+	selectSearchInput = (e) => {
+		if (e.keyCode === 83) {
+			this.searchInput.current.focus();
+		}
+	}
+
+	handleFilterTextChange = (e) => {
+		this.props.onFilterTextChange(e.target.value);
+		(e.target.value) ? this.ClearBtnCL.add('show') : this.ClearBtnCL.remove('show');
+	}
+
+	handleClearInput = (e) => {
+		e.preventDefault();
+		this.searchInput.current.value = '';
+		this.searchInput.current.blur();
+		this.props.onFilterTextClear();
+		this.ClearBtnCL.remove('show');
+	}
+
+	handleKeyUp = (e) => {
+		if (e.keyCode === 27) {
+			this.handleClearInput(e);
+		}
+	}
+
+	handleFocus = () => {
+		if (!this.SearchInputCL.contains('focused')) this.SearchInputCL.add('focused');
+	}
+
+	handleBlur = () => {
+		if (this.SearchInputCL.contains('focused')) this.SearchInputCL.remove('focused');
+	}
+
 	componentDidMount() {
 		window.addEventListener('keyup', this.selectSearchInput);
 		this.ClearBtnCL = document.querySelector('.ClearButton').classList;
@@ -34,44 +61,11 @@ class SearchBar extends Component {
 		window.removeEventListener('keyup', this.selectSearchInput);
 	}
 
-	selectSearchInput(e) {
-		if (e.keyCode === 83) {
-			this.searchInput.current.focus();
-		}
-	}
-
-	handleFilterTextChange(e) {
-		this.props.onFilterTextChange(e.target.value);
-		(e.target.value) ? this.ClearBtnCL.add('show') : this.ClearBtnCL.remove('show');
-	}
-
-	handleClearInput(e) {
-		e.preventDefault();
-		this.searchInput.current.value = '';
-		this.searchInput.current.blur();
-		this.props.onFilterTextClear();
-		this.ClearBtnCL.remove('show');
-	}
-
-	handleKeyUp(e) {
-		if (e.keyCode === 27) {
-			this.handleClearInput(e);
-		}
-	}
-
-	handleFocus() {
-		if (!this.SearchInputCL.contains('focused')) this.SearchInputCL.add('focused');
-	}
-
-	handleBlur() {
-		if (this.SearchInputCL.contains('focused')) this.SearchInputCL.remove('focused');
-	}
-
 	render() {
 		return (
-			<form className="SearchBar" 
+			<form className="SearchBar"
 				onSubmit={e => this.handleClearInput(e)}>
-				<input type="text" 
+				<input type="text"
 					placeholder="Search Emoticons..."
 					value={this.props.filterText}
 					onChange={this.handleFilterTextChange}
