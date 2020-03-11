876175ed3296cd618e13fbbc8e110b10bacdb848
Daniel Dogeanu <contact@danieldogeanu.com>
2020-03-11 17:02:02 +0200

    Moved SelectedScreen to Elements

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import emojiJSON from '../emoji.json'; // NOTE: Emoji List Source: https://github.com/amio/emoji.json
-import IconsSprite from '../elements/IconsSprite';
-import TopBar from '../elements/TopBar.js';
 import SearchBar from './SearchBar';
 import ListContainer from './ListContainer.js';
 import ScrollUp from './ScrollUp';
 import ShortcutsButton from './ShortcutsButton';
 import ShortcutsScreen from './ShortcutsScreen';
-import SelectedScreen from './SelectedScreen';
+import IconsSprite from '../elements/IconsSprite';
+import TopBar from '../elements/TopBar.js';
+import SelectedScreen from '../elements/SelectedScreen';
 import '../styles/components/App.scss';
 
 class App extends Component {
