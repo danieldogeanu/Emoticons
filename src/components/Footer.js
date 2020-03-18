016ede598038cb94737dfd260d61b452090394bb
Daniel Dogeanu <contact@danieldogeanu.com>
2020-03-18 05:27:31 +0200

    Refactored Footer to Use Arrow Function

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,27 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Link from '../elements/Link';
-import details from '../details.json';
+import {author, social} from '../details.json';
 import '../styles/components/Footer.scss';
 
-class Footer extends Component {
-	render() {
-		const {author, social} = details;
-		return (
-			<footer className="Footer">
-				<p className="author">By: <Link data={author} /></p>
-				<p className="mynets">
-					<span className="folw">Follow Me: </span>
-					<span className="wrapper">
-						{social.map(
-							(value) => (<Link
-								key={value.name.toLowerCase()}
-								data={value} social={true} />)
-						)}
-					</span>
-				</p>
-			</footer>
-		);
-	}
-}
+const Footer = () => (
+	<footer className="Footer">
+		<p className="author">By: <Link data={author} /></p>
+		<p className="mynets">
+			<span className="folw">Follow Me: </span>
+			<span className="wrapper">
+				{social.map((value) => (
+					<Link key={value.name.toLowerCase()}
+								data={value} social={true} />
+				))}
+			</span>
+		</p>
+	</footer>
+);
 
 export default Footer;
