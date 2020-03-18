980704f1fbc10dfba74150dc88bc75e99b5e7775
Daniel Dogeanu <contact@danieldogeanu.com>
2020-03-18 05:11:31 +0200

    Refactored Footer and Moved Static Data to Details

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,31 +1,22 @@
 import React, { Component } from 'react';
 import Link from '../elements/Link';
+import details from '../details.json';
 import '../styles/components/Footer.scss';
 
 class Footer extends Component {
-	constructor() {
-		super();
-		this.state = {
-			author: { name: 'Daniel Dogeanu', url: 'https://danieldogeanu.com' },
-			social: [
-				{ name: 'Twitter', url: 'https://twitter.com/danieldogeanu' },
-				{ name: 'GitHub', url: 'https://github.com/danieldogeanu' },
-				{ name: 'Behance', url: 'https://behance.net/danieldogeanu' },
-			]
-		};
-	}
-
 	render() {
-		const {author, social} = this.state;
+		const {author, social} = details;
 		return (
 			<footer className="Footer">
 				<p className="author">By: <Link data={author} /></p>
 				<p className="mynets">
 					<span className="folw">Follow Me: </span>
 					<span className="wrapper">
-						{social.map((value) => {
-							return <Link key={value.name.toLowerCase()} data={value} social={true} />;
-						})}
+						{social.map(
+							(value) => (<Link
+								key={value.name.toLowerCase()}
+								data={value} social={true} />)
+						)}
 					</span>
 				</p>
 			</footer>
