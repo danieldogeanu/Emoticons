fc70fc3213c3bdab5803ca5849f6f79a87fc6462
Daniel Dogeanu <contact@danieldogeanu.com>
2020-04-11 07:43:48 +0300

    Added Test & Snapshot for Name Element

---
+++ src/tests/elements/Name.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {render} from '@testing-library/react';
+import Name from '../../elements/Name';
+
+describe('Name Element', () => {
+
+	it('renders emoticon name properly', () => {
+		const testName = 'Test Emoticon Name';
+		const {getByText} = render(<Name name={testName} />);
+		const renderedName = getByText(testName);
+
+		expect(renderedName).toBeInTheDocument();
+		expect(renderedName).toHaveTextContent(testName);
+		expect(renderedName).toHaveClass('Name');
+		expect(renderedName).toMatchSnapshot();
+	});
+
+});
