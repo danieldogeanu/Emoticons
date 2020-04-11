229ec2d808cf8d5ba73be9965122179195be432f
Daniel Dogeanu <contact@danieldogeanu.com>
2020-04-12 02:05:49 +0300

    Refactored Test for ClearButton

--- src/tests/elements/ClearButton.test.js
+++ src/tests/elements/ClearButton.test.js
@@ -2,27 +2,37 @@ import React from 'react';
 import {render} from '@testing-library/react';
 import ClearButton from '../../elements/ClearButton';
 
+const btnTitle = 'Clear Input';
+
 describe('ClearButton Element', () => {
 
-	it('clear button renders properly', () => {
-		const btnTitle = 'Clear Input';
-		const {getByTitle, getByText, container} = render(<ClearButton />);
+	it('renders clear button properly', () => {
+		const {getByTitle} = render(<ClearButton />);
 		const renderedBtn = getByTitle(btnTitle);
-		const renderedSRTxt = getByText(btnTitle);
-		const renderedSVG = container.querySelector('svg');
 
-		// Full Rendered Button
 		expect(renderedBtn).toBeInTheDocument();
 		expect(renderedBtn).toHaveClass('ClearButton');
-		expect(renderedBtn).toContainElement(renderedSRTxt);
-		expect(renderedBtn).toContainElement(renderedSVG);
 		expect(renderedBtn).toMatchSnapshot();
+	});
 
-		// Screen Reader Text
+	it('has screen reader text', () => {
+		const {getByTitle, getByText} = render(<ClearButton />);
+		const renderedBtn = getByTitle(btnTitle);
+		const renderedSRTxt = getByText(btnTitle);
+
+		expect(renderedBtn).toContainElement(renderedSRTxt);
+
 		expect(renderedSRTxt).toBeInTheDocument();
 		expect(renderedSRTxt).toHaveClass('show-for-screen-reader');
+	});
 
-		// SVG Icon
+	it('has close svg icon', () => {
+		const {getByTitle, container} = render(<ClearButton />);
+		const renderedBtn = getByTitle(btnTitle);
+		const renderedSVG = container.querySelector('svg');
+
+		expect(renderedBtn).toContainElement(renderedSVG);
+
 		expect(renderedSVG).toBeInTheDocument();
 		expect(renderedSVG).toHaveClass('icon', 'close');
 	});
