fcc1d94ee19dd740eec684edb069755ee33faf2c
Daniel Dogeanu <contact@danieldogeanu.com>
2020-04-26 23:29:32 +0300

    Refactored ScreenReaderText Test

--- src/tests/elements/ScreenReaderText.test.js
+++ src/tests/elements/ScreenReaderText.test.js
@@ -2,10 +2,11 @@ import React from 'react';
 import {render} from '@testing-library/react';
 import ScreenReaderText from '../../elements/ScreenReaderText';
 
+const testText = 'Some Test Text';
+
 describe('ScreenReaderText Element', () => {
 
 	it('renders screen reader text properly', () => {
-		const testText = 'Some Test Text';
 		const {getByTestId} = render(<ScreenReaderText text={testText} />);
 		const renderedSRT = getByTestId('ScreenReaderText');
 
