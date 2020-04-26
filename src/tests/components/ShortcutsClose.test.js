878698477e80a7bca9fe7cf9b2827f1520615cf2
Daniel Dogeanu <contact@danieldogeanu.com>
2020-04-27 02:08:00 +0300

    Refactored ShortcutsClose Test

--- src/tests/components/ShortcutsClose.test.js
+++ src/tests/components/ShortcutsClose.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {render, fireEvent, cleanup, waitForDomChange} from '@testing-library/react';
+import {render, fireEvent, waitForDomChange} from '@testing-library/react';
 import ShortcutsClose from '../../components/ShortcutsClose';
 
 const btnTitle = 'Close Shortcuts Screen';
@@ -7,34 +7,45 @@ const compNames = {
 	button: 'ShortcutsButton',
 	screen: 'ShortcutsScreen',
 	close: 'ShortcutsClose',
+	srText: 'ScreenReaderText',
+};
+const classNames = {
+	icon: 'icon',
+	close: 'close',
+	show: 'show',
+	hide: 'hide',
+	animated: 'animated',
+	fadeIn: 'fadeIn',
+	fadeOut: 'fadeOut',
 };
 
 describe('ShortcutsClose Component', () => {
 
-	afterEach(cleanup);
-
 	it('renders shortcuts close button properly', () => {
 		const {getByTitle, getByTestId, container} = render(<ShortcutsClose />);
-		const renderedBtn = getByTitle(btnTitle);
-		const renderedSRText = getByTestId('ScreenReaderText');
-		const renderedIcon = container.querySelector('svg');
+		const rendered = {
+			btn: getByTitle(btnTitle),
+			srText: getByTestId(compNames.srText),
+			icon: container.querySelector('svg'),
+		};
 
-		expect(renderedBtn).toBeInTheDocument();
-		expect(renderedBtn).toHaveClass(compNames.close);
-		expect(renderedBtn).toHaveAttribute('title', btnTitle);
-		expect(renderedBtn).toContainElement(renderedSRText);
-		expect(renderedBtn).toContainElement(renderedIcon);
-		expect(renderedBtn).toMatchSnapshot();
+		expect(rendered.btn).toBeInTheDocument();
+		expect(rendered.btn).toHaveClass(compNames.close);
+		expect(rendered.btn).toHaveAttribute('title', btnTitle);
+		expect(rendered.btn).toContainElement(rendered.srText);
+		expect(rendered.btn).toContainElement(rendered.icon);
+		expect(rendered.btn).toMatchSnapshot();
 
-		expect(renderedSRText).toHaveTextContent(btnTitle);
-		expect(renderedIcon).toHaveClass('icon', 'close');
+		expect(rendered.srText).toHaveTextContent(btnTitle);
+		expect(rendered.icon).toHaveClass(classNames.icon, classNames.close);
 	});
 
 	it('closes shortcuts screen', async () => {
-		const classesToRemove = ['show', 'animated', 'fadeIn', 'fadeOut'];
+		const {show, animated, fadeIn, fadeOut} = classNames;
+		const classesToRemove = [show, animated, fadeIn, fadeOut];
 		const {getByTitle, getByTestId} = render(
 			<div data-testid="test-parent">
-				<button className={[compNames.button, 'hide'].join(' ')}
+				<button className={[compNames.button, classNames.hide].join(' ')}
 					data-testid={compNames.button} />
 				<div className={[compNames.screen, ...classesToRemove].join(' ')}
 					data-testid={compNames.screen}>
@@ -42,15 +53,17 @@ describe('ShortcutsClose Component', () => {
 				</div>
 			</div>
 		);
-		const shortcutsClose = getByTitle(btnTitle);
-		const shortcutsButton = getByTestId(compNames.button);
-		const shortcutsScreen = getByTestId(compNames.screen);
+		const shortcuts = {
+			close: getByTitle(btnTitle),
+			button: getByTestId(compNames.button),
+			screen: getByTestId(compNames.screen),
+		};
 
-		fireEvent.click(shortcutsClose);
+		fireEvent.click(shortcuts.close);
 		await waitForDomChange();
 
-		expect(shortcutsScreen).not.toHaveClass(...classesToRemove);
-		expect(shortcutsButton).not.toHaveClass('hide');
+		expect(shortcuts.screen).not.toHaveClass(...classesToRemove);
+		expect(shortcuts.button).not.toHaveClass(classNames.hide);
 	});
 
 });
