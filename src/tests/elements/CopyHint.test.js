e82a43fad002de55b77826ed2d254dbd6cb28b7f
Daniel Dogeanu <contact@danieldogeanu.com>
2020-04-26 22:53:09 +0300

    Refactored CopyHint Test

--- src/tests/elements/CopyHint.test.js
+++ src/tests/elements/CopyHint.test.js
@@ -2,10 +2,11 @@ import React from 'react';
 import {render} from '@testing-library/react';
 import CopyHint from '../../elements/CopyHint';
 
+const testHint = 'Test Hint';
+
 describe('CopyHint Element', () => {
 
 	it('renders copy hint element properly', () => {
-		const testHint = 'Test Hint';
 		const {getByText} = render(<CopyHint text={testHint} />);
 		const renderedHint = getByText(testHint);
 
