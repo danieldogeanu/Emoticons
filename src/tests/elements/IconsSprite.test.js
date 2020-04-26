f3fef65561095a918eb8f155526deef26763c7bf
Daniel Dogeanu <contact@danieldogeanu.com>
2020-04-26 23:19:40 +0300

    Refactored IconsSprite Test

--- src/tests/elements/IconsSprite.test.js
+++ src/tests/elements/IconsSprite.test.js
@@ -2,19 +2,24 @@ import React from 'react';
 import {render} from '@testing-library/react';
 import IconsSprite from '../../elements/IconsSprite';
 
+const compNames = {
+	sprite: 'IconsSprite',
+};
+
 describe('IconsSprite Element', () => {
 
 	it('renders icons sprite properly', () => {
-		const testId = 'IconsSprite';
 		const {getByTestId, container} = render(<IconsSprite />);
-		const renderedSprite = getByTestId(testId);
-		const renderedSVG = container.querySelector('svg');
+		const rendered = {
+			sprite: getByTestId(compNames.sprite),
+			svg: container.querySelector('svg'),
+		};
 
-		expect(renderedSprite).toBeInTheDocument();
-		expect(renderedSprite).toHaveClass(testId);
-		expect(renderedSprite).toHaveStyle('display: none;');
-		expect(renderedSprite).toContainElement(renderedSVG);
-		expect(renderedSprite).toMatchSnapshot();
+		expect(rendered.sprite).toBeInTheDocument();
+		expect(rendered.sprite).toHaveClass(compNames.sprite);
+		expect(rendered.sprite).toHaveStyle('display: none;');
+		expect(rendered.sprite).toContainElement(rendered.svg);
+		expect(rendered.sprite).toMatchSnapshot();
 	});
 
 });
