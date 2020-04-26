9191933f58e1e4b1ce6e8ecd66110c52275cc1bc
Daniel Dogeanu <contact@danieldogeanu.com>
2020-04-26 23:12:14 +0300

    Refactored Footer Test

--- src/tests/elements/Footer.test.js
+++ src/tests/elements/Footer.test.js
@@ -3,52 +3,65 @@ import {render} from '@testing-library/react';
 import {author, social} from '../../details.json';
 import Footer from '../../elements/Footer';
 
+const compNames = {
+	footer: 'Footer',
+}
+
+const compClass = {
+	mynets: 'mynets',
+	mynet: 'mynet',
+	folw: 'folw',
+	wrapper: 'wrapper',
+	author: 'author',
+};
+
 describe('Footer Element', () => {
 
 	it('renders footer element properly', () => {
 		const {getByTestId} = render(<Footer />);
-		const renderedFooter = getByTestId('Footer');
+		const renderedFooter = getByTestId(compNames.footer);
 
 		expect(renderedFooter).toBeInTheDocument();
-		expect(renderedFooter).toHaveClass('Footer');
+		expect(renderedFooter).toHaveClass(compNames.footer);
 		expect(renderedFooter).toMatchSnapshot();
 	});
 
 	it('contains author section', () => {
 		const {getByTestId} = render(<Footer />);
-		const renderedAuthor = getByTestId('author');
+		const renderedAuthor = getByTestId(compClass.author);
 		const renderedLink = renderedAuthor.querySelector('a');
 
 		expect(renderedAuthor).toBeInTheDocument();
-		expect(renderedAuthor).toHaveClass('author');
+		expect(renderedAuthor).toHaveClass(compClass.author);
 		expect(renderedAuthor).toHaveTextContent('By: ' + author.name);
 		expect(renderedAuthor).toContainElement(renderedLink);
 		expect(renderedLink).toHaveAttribute('href', author.url);
 	});
 
 	it('contains social section', () => {
-		const netsClass = 'mynets';
 		const {getByTestId, getByText, getAllByTestId} = render(<Footer />);
-		const renderedNets = getByTestId(netsClass);
-		const renderedFollow = getByText(/follow me/i);
-		const renderedWrapper = getByTestId('wrapper');
-		const renderedLinks = getAllByTestId('mynet');
+		const rendered = {
+			nets: getByTestId(compClass.mynets),
+			follow: getByText(/follow me/i),
+			wrapper: getByTestId(compClass.wrapper),
+			links: getAllByTestId(compClass.mynet),
+		};
 
-		expect(renderedNets).toBeInTheDocument();
-		expect(renderedNets).toHaveClass(netsClass);
-		expect(renderedNets).toContainElement(renderedFollow);
-		expect(renderedNets).toContainElement(renderedWrapper);
+		expect(rendered.nets).toBeInTheDocument();
+		expect(rendered.nets).toHaveClass(compClass.mynets);
+		expect(rendered.nets).toContainElement(rendered.follow);
+		expect(rendered.nets).toContainElement(rendered.wrapper);
 
-		expect(renderedFollow).toHaveClass('folw');
-		expect(renderedFollow).toHaveTextContent('Follow Me:');
+		expect(rendered.follow).toHaveClass(compClass.folw);
+		expect(rendered.follow).toHaveTextContent('Follow Me:');
 
-		expect(renderedWrapper).toHaveClass('wrapper');
-		expect(renderedLinks.length).toBe(4);
+		expect(rendered.wrapper).toHaveClass(compClass.wrapper);
+		expect(rendered.links.length).toBe(4);
 	});
 
 	it('contains all social networks', () => {
 		const {getAllByTestId} = render(<Footer />);
-		const renderedNets = getAllByTestId('mynet');
+		const renderedNets = getAllByTestId(compClass.mynet);
 
 		renderedNets.forEach((net, i) => {
 			expect(net).toHaveTextContent(social[i].name);
