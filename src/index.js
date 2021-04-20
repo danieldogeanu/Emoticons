bd500c024329ee8cc0e5cf0ae61fc391675d5c48
Daniel Dogeanu <contact@danieldogeanu.com>
2021-04-21 02:26:10 +0300

    Converted Back to Create React App

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import BrowserDetection from 'react-browser-detection';
-import './styles/index.scss';
 import App from './components/App';
-import * as serviceWorker from './serviceWorker';
+import './styles/index.scss';
 
 const browserHandler = {
 	ie: () => false,
@@ -25,8 +24,3 @@ ReactDOM.render(
 	<BrowserDetection>{browserHandler}</BrowserDetection>,
 	document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
