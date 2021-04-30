f156e28d2fc1a7256216832a635c077ad20d6ca1
Daniel Dogeanu <contact@danieldogeanu.com>
2021-05-01 01:12:28 +0300

    Converted ListContainer to Use React Window

--- src/components/ListContainer.js
+++ src/components/ListContainer.js
@@ -1,10 +1,29 @@
-import React, { Component } from 'react';
+import React, { Component, PureComponent } from 'react';
 import SimpleBar from 'simplebar-react';
+import { FixedSizeList } from 'react-window';
 import ListItem from './ListItem';
 import Footer from '../elements/Footer';
 import Labels from '../elements/Labels';
 import '../styles/components/ListContainer.scss';
 import 'simplebar/dist/simplebar.min.css';
+
+class DesktopListRow extends PureComponent {
+	render() {
+		const {index, style, data} = this.props;
+		const emoticon = data[index];
+
+		return <ListItem type="desktop" data={emoticon} style={style} />;
+	}
+}
+
+class MobileListRow extends PureComponent {
+	render() {
+		const {index, style, data} = this.props;
+		const emoticon = data[index];
+
+		return <ListItem type="mobile" data={emoticon} style={style} />;
+	}
+}
 
 class ListContainer extends Component {
 	constructor(props) {
@@ -26,6 +45,15 @@ class ListContainer extends Component {
 		}, 200);
 	}
 
+	generateKey = (index, data) => {
+		const emoticon = data[index];
+		return emoticon.codes.split(' ').join('');
+	}
+
+	filterData = (data, filter) => data.filter(emoticon => {
+		return (emoticon.name.toLowerCase().indexOf(filter.toLowerCase()) !== -1);
+	});
+
 	componentDidMount() {
 		window.addEventListener('resize', this.handleResize);
 		this.setState({emoticonsNumber: this.props.data.length});
@@ -39,18 +67,10 @@ class ListContainer extends Component {
 		const {data, filterText} = this.props;
 		const {isMobile} = this.state;
 
-		const filteredData = data.filter(emoticon => {
-			return (emoticon.name.toLowerCase().indexOf(filterText.toLowerCase()) !== -1);
-		});
-
-		const mobileItems = [];
-		const desktopItems = [];
-
-		filteredData.forEach(emoticon => {
-			let key = emoticon.codes.split(' ').join('');
-			mobileItems.push(<ListItem type="mobile" key={key} data={emoticon} />);
-			desktopItems.push(<ListItem type="desktop" key={key} data={emoticon} />);
-		});
+		const filteredData = this.filterData(data, filterText);
+		const numRows = filteredData.length;
+		const rowHeight = isMobile ? 90 : 50;
+		const totalHeight = rowHeight * numRows;
 
 		return (
 			<div className="ListContainer"
@@ -60,9 +80,18 @@ class ListContainer extends Component {
 					scrollableNodeProps={{ref: this.list}}>
 					<div className="innerWrapper">
 						<Labels />
-						<ul data-testid="listUl">
-							{isMobile ? mobileItems : desktopItems}
-						</ul>
+						<FixedSizeList
+							data-testid="listUl"
+							innerElementType="ul"
+							itemKey={this.generateKey}
+							itemData={filteredData}
+							itemSize={rowHeight}
+							itemCount={numRows}
+							height={totalHeight}
+							width="100%"
+						>
+							{isMobile ? MobileListRow : DesktopListRow}
+						</FixedSizeList>
 						<Footer />
 					</div>
 				</SimpleBar>
--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -7,13 +7,13 @@ import CopyHint from '../elements/CopyHint';
 import '../styles/components/ListItem.scss';
 
 function ListItem(props) {
-	const {data, type} = props;
+	const {data, type, style} = props;
 	const {char, name} = data;
 
 	switch (type) {
 		case 'desktop':
 			return (
-				<li className="DesktopListItem" data-testid="DesktopListItem">
+				<li className="DesktopListItem" data-testid="DesktopListItem" style={style}>
 					<Emoticon char={char} name={name} />
 					<Name name={name} />
 					<Copy>
@@ -23,7 +23,7 @@ function ListItem(props) {
 			);
 		case 'mobile':
 			return (
-				<li className="MobileListItem" data-testid="MobileListItem">
+				<li className="MobileListItem" data-testid="MobileListItem" style={style}>
 					<CopyButton data={char} className="mobileCard ripple">
 						<Name name={name} />
 						<div className="bottomBar">
